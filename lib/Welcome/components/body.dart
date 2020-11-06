154ddd3d671ebd9726133a3a80685f4cef9af25b
mikhaelsuryawan <mikhaelsuryawan@gmail.com>
2020-11-06 22:15:23 +0700

    clear

--- lib/Welcome/components/body.dart
+++ lib/Welcome/components/body.dart
@@ -91,7 +91,6 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Background(
       child: SingleChildScrollView(
         child: Column(
